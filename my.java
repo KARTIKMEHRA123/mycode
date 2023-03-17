cf022f96f225c3dc8842e68ba5cbc0220f5dc541
KARTIKMEHRA123 <mehrakartik136@gmail.com>
2023-03-17 22:40:28 +0530

    first commit

---
+++ my.java
@@ -0,0 +1,13 @@
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+cSystem.out.println("Hello, World!"); 
